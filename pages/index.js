25f9135307da18653199713fe1e85329127a4f3b
Yury Shevchenko <shevchenko_yury@mail.ru>
2019-12-11 10:46:50 +0100

    changed index

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,12 @@ import RecordComponent from '../components/Record';
 import React from 'react';
 import * as superagent from 'superagent';
 import styled from "styled-components";
-const mongoose = require('mongoose')
-const Record = mongoose.model('Record')
 
 export default class extends React.Component {
-
   constructor () {
     super()
     this.state = {}
   }
-
-
   render () {
     return (
       <Layout>
